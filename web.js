1e20ec50a2e35493361e43b8aa57e0e6fb50cf8a
Gustavo Recio <gustavo.recio@gmail.com>
2013-07-15 19:34:48 +0000

    First attempt at reading from index.html

--- web.js
+++ web.js
@@ -4,6 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+  var indexBuffer = fs.readFile('index.html');
+  response.send(indexBuffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
